faae97d4d583e335671c7a4d203323683c224e02
Yash Bansal <yashbansal1710@email.com>
2025-07-09 17:23:58 +0530

    api url update

--- react-frontend/src/config.js
+++ react-frontend/src/config.js
@@ -1,2 +1,2 @@
-export const API_BASE_URL = "https://coffee-shop-41dh.onrender.com";
+export const API_BASE_URL = "https://coffee-shop-0sfb.onrender.com";
 // export const API_BASE_URL = "http://localhost:5000";
